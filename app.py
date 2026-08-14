e85414a4bfe69363f08cac012eb1c65351955f04
Hamze Abdi <441105866@s.mu.edu.sa>
2026-08-14 21:32:07 +0300

    Fix Render startup

--- app.py
+++ app.py
@@ -390,12 +390,10 @@ def certificate(id):
 
 if __name__ == "__main__":
 
-
     update_database()
-
 
     app.run(
         host="0.0.0.0",
-        port=5000,
+        port=int(os.environ.get("PORT", 5000)),
         debug=True
     )
